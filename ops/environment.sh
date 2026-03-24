f2451c44070e31f8f3485b79ae037412a98d064c
Jeán Thysse <jt@jkthysse.com>
2026-03-25 00:20:40 +0200

    Introduces k3d for local cluster deployment

--- ops/environment.sh
+++ ops/environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ENV_FILE="${SCRIPT_DIR}/.env"
-ENV_EXAMPLE_FILE="${SCRIPT_DIR}/.env.example"
+_OPS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ENV_FILE="${_OPS_DIR}/.env"
+ENV_EXAMPLE_FILE="${_OPS_DIR}/.env.example"
 
 if [[ ! -f "${ENV_FILE}" ]]; then
   echo "Error: .env not found. Copy ${ENV_EXAMPLE_FILE} to ${ENV_FILE} and fill in the desired configuration values:"
